1fe497a68b34ec80c6462e34a6106bcaf875e694
DESKTOP-FU5IR4A\kminju <minju987@naver.com>
2024-11-05 17:54:58 +0900

    finish area select

--- Eats/src/main/java/com/eats/mapper/user/MainMapper.java
+++ Eats/src/main/java/com/eats/mapper/user/MainMapper.java
@@ -2,6 +2,7 @@ package com.eats.mapper.user;
 
 import java.util.List;
 
+import com.eats.user.model.AreaDTO;
 import com.eats.user.model.CateKeyDTO;
 import com.eats.user.model.CateValueDTO;
 
@@ -9,4 +10,7 @@ public interface MainMapper {
 
 	public List<CateKeyDTO> getCateKey();
 	public List<String> getValueList(int cate_key_idx);
+	
+	public List<AreaDTO> getCityList();
+	public List<String> getUnitList(int area_idx);
 }
