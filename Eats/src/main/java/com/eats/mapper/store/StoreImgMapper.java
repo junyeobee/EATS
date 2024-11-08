024a0f0dd00edd677f51c86e890c19f420f8d16a
DESKTOP-QJ5JAF6\SUA <dlatndk2160@naver.com>
2024-11-08 14:39:42 +0900

    modify/storeImg/jsp/saveRead

--- Eats/src/main/java/com/eats/mapper/store/StoreImgMapper.java
+++ Eats/src/main/java/com/eats/mapper/store/StoreImgMapper.java
@@ -1,13 +1,11 @@
 package com.eats.mapper.store;
 
-import java.io.IOException;
-import java.util.List;
-
-import org.springframework.web.multipart.MultipartFile;
 
 import com.eats.store.model.StoreImgDTO;
 
 public interface StoreImgMapper {
 	public int storeImgInsert(StoreImgDTO dto);
-	public StoreImgDTO storeImgData(int store_idx);
+	public StoreImgDTO storeImgData();
+	public int storeImgUpdate(StoreImgDTO dto);
+	public int storeImgUpDel(StoreImgDTO dto);
 }
