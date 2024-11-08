4abe445d1c8ea97a5cb5c07343be93594d9c6977
DESKTOP-I4Q2UMB\user <hahaha2227@naver.com>
2024-11-08 18:04:14 +0900

    dd

--- Eats/src/main/java/com/eats/user/service/KakaoLoginServiceImple.java
+++ Eats/src/main/java/com/eats/user/service/KakaoLoginServiceImple.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.eats.kakao.model.KakaoUserDTO;
 import com.eats.mapper.user.KakaoUserMapper;
@@ -15,23 +16,24 @@ public class KakaoLoginServiceImple implements KakaoLoginService {
 	@Autowired
 	private KakaoUserMapper mapper;
 	
+	public KakaoLoginServiceImple() {
+		
+	}
+	
+	public KakaoLoginServiceImple(KakaoUserMapper mapper) {
+		this.mapper=mapper;
+	}
+	
 	@Override
 	public EatsUserDTO checkUserIdx(String kakaoId) {
 		EatsUserDTO dto=mapper.kakaoIdCheck(kakaoId);
-		
 		return dto;
 	}
 
 	@Override
-	public int insertKakaoUser(KakaoUserDTO dto) {
+	@Transactional
+	public void insertUserAndProfile(EatsUserDTO userDTO) {
 		
-		int result=mapper.insertUser(dto);
-		return result;
-	}
-
-	@Override
-	public int insertUserProfile(Map map) {
-		int result=mapper.insertUserProfile(map);
-		return result;
+		//mapper.insertUser(userDTO);
 	}
 }
