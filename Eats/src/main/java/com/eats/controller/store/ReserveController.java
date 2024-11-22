417798af307d0a47548b0274076c20f548b68a32
microsoft\SIST60 <narosong@gmail.com>
2024-11-22 13:50:01 +0900

    d

--- Eats/src/main/java/com/eats/controller/store/ReserveController.java
+++ Eats/src/main/java/com/eats/controller/store/ReserveController.java
@@ -3,6 +3,7 @@ package com.eats.controller.store;
 import java.sql.Date;
 import java.text.DecimalFormat;
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -18,7 +19,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.eats.store.model.reserve.StoreTimeDTO;
-import com.eats.store.model.SalesResponseDTO;
 import com.eats.store.model.reserve.ReservOkViewDTO;
 import com.eats.store.model.reserve.ReserveListDTO;
 import com.eats.store.model.reserve.TableDTO;
@@ -96,35 +96,71 @@ public class ReserveController {
 	}
 
 	private List<String> generateTimeList(String startTime, String endTime, String breakTime) {
-	   List<String> timeList = new ArrayList<>();
-	   
-	   
-       LocalTime time = LocalTime.parse(startTime);
-       LocalTime end = LocalTime.parse(endTime);
-       
-       String[] breakTimes = breakTime.split("-");
-       LocalTime break1 = LocalTime.parse(breakTimes[0]);
-       LocalTime break2 = LocalTime.parse(breakTimes[1]);
-       
-       while (!time.isAfter(end)) {
-           if (time.isBefore(break1) || time.isAfter(break2)) {
-               timeList.add(time.format(DateTimeFormatter.ofPattern("HH:mm")));
-           }
-           time = time.plusMinutes(30);
-       }
-       
-       return timeList;
+		List<String> timeList = new ArrayList<>();
+
+
+		LocalTime time = LocalTime.parse(startTime);
+		LocalTime end = LocalTime.parse(endTime);
+
+		String[] breakTimes = breakTime.split("-");
+		LocalTime break1 = LocalTime.parse(breakTimes[0]);
+		LocalTime break2 = LocalTime.parse(breakTimes[1]);
+		break2 = break2.plusMinutes(-30); //브레이크 타임 끝 시간부터
+		end = end.plusHours(-1); //마감 1시간전까지
+		while (!time.isAfter(end)) {
+			if (time.isBefore(break1) || time.isAfter(break2)) {
+				timeList.add(time.format(DateTimeFormatter.ofPattern("HH:mm")));
+			}
+			time = time.plusMinutes(30);
+		}
+
+		return timeList;
 	}
 
 	@GetMapping("/reserveDateInTime")
 	@ResponseBody
-	public StoreTimeDTO getTimeListOfDate(HttpServletRequest req, String date) {
+	public List<String> getTimeListOfDate(HttpServletRequest req, String date) {
+		HttpSession session = req.getSession();
+		LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+		String todayChar = switch(dayOfWeek) {
+			case MONDAY -> "월";
+			case TUESDAY -> "화";
+			case WEDNESDAY -> "수";
+			case THURSDAY -> "목";
+			case FRIDAY -> "금";
+			case SATURDAY -> "토";
+			case SUNDAY -> "일";
+		};
+		int storeIdx = session.getAttribute("store_idx") == null ? 1 : (Integer)session.getAttribute("store_idx");
+		Map<String, Object> map = new HashMap<>();
+		map.put("storeIdx", storeIdx);
+		map.put("stimeDay", todayChar);
+		StoreTimeDTO timeInfo = service.storeTime(map);
+		if(timeInfo == null) {
+			List<String> emptyList = new ArrayList<>();
+			emptyList.add("휴업일이거나 설정되지 않았습니다.");
+			return emptyList;
+		}
+		List<String> timeList = generateTimeList(timeInfo.getStime_start(),timeInfo.getStime_end(),timeInfo.getStime_break());
+		return timeList;
+	}
+
+	@GetMapping("/reservations")
+	@ResponseBody
+	public ReservOkViewDTO getReservations(HttpServletRequest req, String date, String time) {
 		HttpSession session = req.getSession();
 		int storeIdx = session.getAttribute("store_idx") == null ? 1 : (Integer)session.getAttribute("store_idx");
-		Map map = new HashMap<>();
+		Map<String, Object> map = new HashMap<>();
 		map.put("storeIdx", storeIdx);
-		map.put("date", date);
-		StoreTimeDTO timeInfo = service.storeTime(map);
-		return timeInfo;
+		map.put("day", date);
+		map.put("time", time);
+		List<ReserveListDTO> lists = service.reserveList(map);
+		
+	   	List<TableDTO> tables = service.tables(map);
+		ReservOkViewDTO dto = new ReservOkViewDTO();
+		dto.setTables(tables);
+		dto.setLists(lists);
+		return dto;
 	}
 }
